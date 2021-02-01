9d2193e84d4a339962350591cc9860ae7f77a450
matesz44 <matesz44>
2021-02-01 23:37:12 +0100

    html file parsing to nodes works!

--- parse.go
+++ parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link represents a link (<a href="..">) in an HTML document
 type Link struct {
@@ -11,5 +16,17 @@ type Link struct {
 // Parse will take in an HTML document and will return a
 // slice of links parsed from it
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	fmt.Println(padding, n.Data)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
