9a8a2ecaa6658de55d50e10f4d4ae790be4ac5f7
matesz44 <matesz44>
2021-02-03 19:04:57 +0100

    added recursion

--- examples/ex2/main.go
+++ examples/ex2/main.go
@@ -16,23 +16,58 @@ import (
 	1. [X] GET req to url
 	2. [X] Parse links on the page with `link`
 	3. [X] Build absolute URL's from relative links
-	4. [/] Filter links to different domains
-	5. [ ] Find all the pages recursively (BFS)
+	4. [X] Filter links to different domains
+	5. [X] Find all the pages recursively (BFS)
 	6. [ ] Output XML
 */
 
 func main() {
 	urlFlag := flag.String("u", "https://m4t3sz.gitlab.io/bsc/", "url you want to crawl")
+	depthFlag := flag.Int("d", 5, "depth you want to follow links")
 	flag.Parse()
 
 	//fmt.Println(*urlFlag)
-	pages := get(*urlFlag)
+	pages := bfs(*urlFlag, *depthFlag)
+	//fmt.Println(depthFlag)
+	//pages := get(*urlFlag)
 	//filter(baseURL)
 
 	for _, page := range pages {
 		fmt.Println(page)
 	}
 
+}
+
+// empty structs need less mem than bools
+type empty struct{}
+
+// all of the urls
+func bfs(urlStr string, depth int) []string {
+	// with a map you dont need to iterate
+	// over the whole thing like with a slice
+	seen := make(map[string]empty)
+	var q map[string]empty
+	nq := map[string]empty{
+		urlStr: struct{}{},
+	}
+	for i := 0; i <= depth; i++ {
+		q, nq = nq, make(map[string]empty)
+		for url := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = empty{}
+
+			for _, link := range get(url) {
+				nq[link] = empty{}
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen))
+	for url := range seen {
+		ret = append(ret, url)
+	}
+	return ret
 }
 
 func get(urlStr string) []string {
@@ -54,7 +89,9 @@ func get(urlStr string) []string {
 	}
 	//fmt.Println("Request URL: ", reqURL.String())
 	//fmt.Println("Base URL:", baseURL.String())
-	return filter(baseURL, hrefs(resp.Body, baseURL))
+	req := reqURL.String()
+	//base := baseURL.String()
+	return filter(hrefs(resp.Body, baseURL), withPrefix(req))
 }
 
 func hrefs(r io.Reader, baseURL *url.URL) []string {
@@ -85,17 +122,22 @@ func hrefs(r io.Reader, baseURL *url.URL) []string {
 	return ret
 }
 
-func filter(baseURL *url.URL, links []string) []string {
-	base := baseURL.String()
+func filter(links []string, keepFn func(string) bool) []string {
 	var ret []string
 	for _, link := range links {
 		// https://m4t3sz.gitlab.io/bsc --> OK
 		// https://m4t3sz.gitlab.io/bsc/writeup --> OK
 		// https://twitter.com --> NOPE
-		if strings.HasPrefix(link, base) {
+		if keepFn(link) {
 			ret = append(ret, link)
 		}
 	}
 
 	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
 }
